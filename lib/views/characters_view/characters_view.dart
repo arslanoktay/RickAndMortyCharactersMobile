f641534c8def30bd7fb10f4408a1a096f1c8d2d5
Oktayy16 <oktay16arslan@hotmail.com>
2024-11-14 13:04:42 +0300

    input text hazır karakter container tasarlanmaya devam ediyor. api sitesinde sorun var sanırım kontrol et

--- lib/views/characters_view/characters_view.dart
+++ lib/views/characters_view/characters_view.dart
@@ -6,17 +6,24 @@ class CharactersView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 17),
+          padding: const EdgeInsets.symmetric(horizontal: 17),
           child: Column( // alt alta geleceği için
             children: [
-              SizedBox(height: 12,),
-              TextField( // Arama yeri
-                decoration: InputDecoration(
-                  labelText: "Karakterlerde Ara",
-                  border: OutlineInputBorder()
+              _searchInputWidget(context),
+              Container(
+                height: 100,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(6)
+                ),
+                child: Row(
+                  children: [
+                    Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9hTWc3Ak2YM7wo6D5RItxI_3jIWipI6lYUQ&s') // doğru image değil ölçeklendirme hatası olabilir sayfaya bağlanmıyor
+                  ],
                 ),
               )
             ],
@@ -24,5 +31,20 @@ class CharactersView extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Padding _searchInputWidget(BuildContext context) {
+    return Padding(
+              padding: const EdgeInsets.only(top: 12,bottom: 16),
+              child: TextField( // Arama yeri
+                decoration: InputDecoration(
+                  labelText: "Karakterlerde Ara",
+                  labelStyle: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+                  border: const OutlineInputBorder(),
+                  prefixIcon: const Icon(Icons.search), // en sola vermek için prefix kullandık sıralamada sağda çünkü
+                  suffixIcon: IconButton(onPressed: (){}, icon: const Icon(Icons.more_vert)) // Sağ tarafa eklemesi için suffix
+                ),
+              ),
+            );
   }
 }
